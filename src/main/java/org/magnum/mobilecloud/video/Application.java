6c928665b545d29fc5d0d918a21a27e80a886489
Manuel Lorenzo <manulorenzop@gmail.com>
2020-03-06 16:59:59 +0100

    make all the tests pass

--- src/main/java/org/magnum/mobilecloud/video/Application.java
+++ src/main/java/org/magnum/mobilecloud/video/Application.java
@@ -2,13 +2,11 @@ package org.magnum.mobilecloud.video;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.magnum.mobilecloud.video.auth.OAuth2SecurityConfiguration;
-import org.magnum.mobilecloud.video.repository.VideoRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -30,7 +28,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 // as part of this configuration so that we can have security and oauth
 // setup by Spring
 @Import(OAuth2SecurityConfiguration.class)
-@EnableJpaRepositories(basePackageClasses = VideoRepository.class)
+//@EnableJpaRepositories(value = "org.magnum.mobilecloud.video.repository")
 public class Application extends RepositoryRestMvcConfiguration {
 
 	// The app now requires that you pass the location of the keystore and
--- src/main/java/org/magnum/mobilecloud/video/repository/VideoRepository.java
+++ src/main/java/org/magnum/mobilecloud/video/repository/VideoRepository.java
@@ -2,16 +2,16 @@ package org.magnum.mobilecloud.video.repository;
 
 import org.magnum.mobilecloud.video.client.VideoSvcApi;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import retrofit.http.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Collection;
 
-@Repository
+@RepositoryRestResource(path = VideoSvcApi.VIDEO_SVC_PATH)
 public interface VideoRepository extends CrudRepository<Video, Long> {
     // GET /video/search/findByName?title={title}
-    Collection<Video> findByName(@Query(VideoSvcApi.TITLE_PARAMETER) String title);
+    Collection<Video> findByName(@Param(VideoSvcApi.TITLE_PARAMETER) String title);
 
     // GET /video/search/findByDurationLessThan?duration={duration}
-    Collection<Video> findByDurationLessThan(@Query(VideoSvcApi.DURATION_PARAMETER) String duration);
+    Collection<Video> findByDurationLessThan(@Param(VideoSvcApi.DURATION_PARAMETER) long duration);
 }
